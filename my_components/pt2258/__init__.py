ab290fa47b1d8b947cfeda72a99a9d766bce2d31
hrch3k <duleml@gmail.com>
2025-03-08 12:32:47 +0100

    Add Files via upload

---
+++ my_components/pt2258/__init__.py
@@ -0,0 +1,40 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import i2c
+from esphome.const import CONF_ID
+
+
+# Declare the dependencies of the component (i2c in this case)
+DEPENDENCIES = ["i2c"]
+CODEOWNERS = ["@hrch3k"]
+
+
+# Establish a namespace for the PT2323 component
+pt2258_ns = cg.esphome_ns.namespace("pt2258")
+
+# Define the PT2258Component class, inheriting from PollingComponent and I2CDevice
+PT2258Component = pt2258_ns.class_("PT2258Component", cg.PollingComponent, i2c.I2CDevice)
+
+
+# Define the configuration schema for the component
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            # Generate an ID for the component
+            cv.GenerateID(): cv.declare_id(PT2258Component),
+            # Add any configuration parameters here if needed
+        }
+    )
+    .extend(cv.polling_component_schema("60s")) # Set polling interval
+    .extend(i2c.i2c_device_schema(0x46))  # PT2323 I2C address
+)
+
+# Function to convert YAML configuration to C++ code
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])  # Create a new variable of PT2258Component type
+
+    # Register the component with its base classes
+    await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
+
+    # Add logic here to handle any additional configuration parameters
